d928e6ed3563021b0d929b82e9eeeeed1a65c57a
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-12-23 20:16:15 +0200

    Use state machine for GameRules states, Obsolete EGameState

--- GameRules/States.cs
+++ GameRules/States.cs
@@ -6,8 +6,8 @@ namespace Amper.FPS;
 
 partial class GameRules
 {
-	[Net] public TimeSince TimeSinceStateChange { get; set; }
-	[Net] public BaseGameState State { get; set; }
+	[Net] public TimeSince TimeSinceStateChange { get; private set; }
+	[Net] public BaseGameState State { get; private set; }
 	BaseGameState LastState { get; set; }
 
 	public virtual void ChangeStateTo( BaseGameState state )
@@ -16,131 +16,30 @@ partial class GameRules
 		State = state;
 	}
 
-	public virtual void SimulatePreGame()
+	void UpdateState()
 	{
-		// If we have players that are ready to play, 
-		// start waiting for players timer.
-		if ( HasPlayers() && WaitingForPlayersEnabled() ) 
+		if ( LastState != State )
 		{
-			StartWaitingForPlayers();
+			// Invoke ended callback on our previous state, if exists.
+			LastState?.OnEnd();
+
+			// Invoke start callback on our new state
+			State?.OnStart();
 		}
-	}
 
-	public virtual void StartedPreGame()
-	{
-		EventDispatcher.InvokeEvent<GameRestartEvent>();
-	}
-
-	public virtual void EndedPreGame() { }
-
-	//
-	// ReadyUp
-	//
-
-	public virtual void SimulateReadyUp() { }
-	public virtual void StartedReadyUp() { }
-	public virtual void EndedReadyUp() { }
-
-	//
-	// PreRound
-	//
-
-	public virtual void SimulatePreRound() 
-	{
-		if ( TimeSinceStateChange > GetPreRoundFreezeTime() )
-		{
-			TransitionToState( EGameState.Gameplay );
-		}
-	}
-
-	public virtual void StartedPreRound()
-	{
-		EventDispatcher.InvokeEvent<RoundRestartEvent>();
-	}
-
-	public virtual void EndedPreRound() { }
-	public virtual float GetPreRoundFreezeTime() { return 6; }
-
-	//
-	// Gameplay
-	//
-
-	public virtual void StartedGameplay()
-	{
-		EventDispatcher.InvokeEvent<RoundActiveEvent>();
-	}
-
-	public virtual void SimulateGameplay() { }
-	public virtual void EndedGameplay() { }
-
-	//
-	// TeamWin
-	//
-
-	public virtual void SimulateRoundEnd()
-	{
-		if ( TimeSinceStateChange > mp_chattime )
-			RestartRound();
-	}
-
-	public virtual void StartedRoundEnd()
-	{
-		EventDispatcher.InvokeEvent<RoundEndEvent>();
-	}
-
-	public virtual void EndedRoundEnd() { }
-
-	//
-	// GameOver
-	//
-
-	public virtual void StartedGameOver()
-	{
-		EventDispatcher.InvokeEvent<GameOverEvent>();
-	}
-
-	void InvokeStateUpdate( BaseGameState state )
-	{
-	}
-
-	void InvokeStateStarted( BaseGameState state )
-	{
-	}
-
-	void InvokeStateEnded( BaseGameState state )
-	{
+		// Invoke update on the state if needed.
+		State?.Update();
 	}
 }
 
 [Obsolete]
 public enum EGameState
 {
-	/// <summary>
-	/// The game was just initialized.
-	/// </summary>
 	Initialized,
-	/// <summary>
-	/// No players have yet joined the game. We are waiting for someone to join.
-	/// </summary>
 	PreGame,
-	/// <summary>
-	/// The game is about to start, we are waiting for players to get ready
-	/// </summary>
 	ReadyUp,
-	/// <summary>
-	/// The round is about to start, we make a short freezetime before the game is on.
-	/// </summary>
 	PreRound,
-	/// <summary>
-	/// Round is on, playing normally.
-	/// </summary>
 	Gameplay,
-	/// <summary>
-	/// The round has ended
-	/// </summary>
 	RoundEnd,
-	/// <summary>
-	/// Game is over, showing the scoreboard etc
-	/// </summary>
 	GameOver
 }
